550246bc53725ef188fcea54174f559dd73cd135
Stranger <rooderuler@gmail.com>
2021-10-26 20:03:56 +0300

    Код написан до времени ролика - 11:19

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Card, {CardVariant} from "./components/Card";
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Card onClick={
+        (num) => console.log('Click !', num)}
+            width='200px'
+            height='200px'
+            variant={CardVariant.primary}>
+        <button>Кнопка</button>
+        <div>qqqq</div>
+      </Card>
     </div>
   );
-}
+};
 
 export default App;
